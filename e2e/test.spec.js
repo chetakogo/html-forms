29a60a5cf6b8ffaef59c3956be27528fc90e7fc1
Liubov Chudakova <chudakova.lyuba.00@gmail.com>
2022-07-20 02:11:13 +0300

    fix test

--- e2e/test.spec.js
+++ e2e/test.spec.js
@@ -1,23 +1,39 @@
-/* eslint-disable no-unused-vars */
 import puppetteer from 'puppeteer';
+import { fork } from 'child_process';
 
 jest.setTimeout(30000);
 
-describe('popover', () => {
+describe('', () => {
   let browser = null;
   let page = null;
+  let server = null;
   const baseUrl = 'http://localhost:9000';
+
   beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on('error', reject);
+      server.on('message', (message) => {
+        if (message === 'ok') {
+          resolve();
+        }
+      });
+    });
+
     browser = await puppetteer.launch({
-      headless: false,
-      slowMo: 100,
-      devtools: true,
+      headless: false, // show gui
+      slowMo: 500,
+      devtools: true, // show devTools
     });
     page = await browser.newPage();
+    await page.goto(baseUrl);
   });
+
   afterAll(async () => {
     await browser.close();
+    server.kill();
   });
+
   test('test button popover', async () => {
     const button = await page.$('.button_popover');
     await button.click();
